178ece0d64ae49ce62e94bc988e03f26342bd956
BergMichel <stefan.sudy1@gmail.com>
2025-12-17 11:11:14 +0100

    fix tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from "react-dom/client"
 import App from './app/App'
+//import './fonts.css';
+import './styles/tailwind.css';
+import './styles/theme.css';
+import './styles/calendar.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
